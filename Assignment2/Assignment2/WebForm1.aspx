9b911801373b74265179ffa80be37feae590680f
Brandon Zimmerman <brandonzimmerman@live.com>
2015-04-23 12:49:00 -0700

    initial sync
    first sync of assignment 2. Still need to create an inner join to
    display artist name in the show table

---
+++ Assignment2/Assignment2/WebForm1.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Assignment2.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Show Selection</title>
+    <link rel="stylesheet" type="text/css" href="StyleSheet.css" />
+    
+</head>
+<body>
+    
+    <form id="form1" runat="server">
+    <div>
+       <h2><em>Assignment 2</em> Artist and Show Selection</h2>
+        <asp:DropDownList ID="DropDownList1" runat="server" AutoPostBack="true" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged"></asp:DropDownList>
+        <br />
+        <asp:GridView ID="GridView1" runat="server"></asp:GridView>
+    </div>
+    </form>
+
+    <!-- beginning footer -->
+
+    <div id="footer">
+    <ul>
+        <li>Copyright, &copy</li>
+        <li>Rights Reserved</li>
+        <li><a href="http://edison.seattlecentral.edu/~bzimme02/index.html">Brandon Designs</a></li>
+    </ul>
+        </div> 
+
+    <!-- end footer -->
+
+</body>
+</html>
---
+++ Assignment2/Assignment2/WebForm1.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Assignment2
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+     ShowTrackerEntities db = new ShowTrackerEntities();   
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                var artists = from a in db.Artists
+                              orderby a.ArtistName
+                              select new { a.ArtistName, a.ArtistKey };
+                
+                DropDownList1.DataSource = artists.ToList();
+                // ArtistName has to match the a.ArtistName from the above query
+                DropDownList1.DataTextField = "ArtistName";
+                // it will display the ArtistName but store the ArtistKey
+                DropDownList1.DataValueField = "ArtistKey";
+                // not sure why this is needed but ask rick> or look online
+                DropDownList1.DataBind();
+            }
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int key = int.Parse(DropDownList1.SelectedValue.ToString());
+            var shows = from s in db.Shows
+                        // create an inner join here
+                        //from a in s.
+                        orderby s.ShowName
+                        // why can't I reference the above a.ArtistName???
+                        //where a.ArtistKey == key
+                        where s.ShowKey == key
+                        select new
+                        {
+                            s.ShowName,
+                            //a.ArtistName,
+                            s.ShowDate,
+                            s.ShowTime
+                        };
+            GridView1.DataSource = shows.ToList();
+            GridView1.DataBind();
+        }
+    }
+}
